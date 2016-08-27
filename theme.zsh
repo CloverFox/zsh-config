f3b33fa70ccdf63c607c5ab0146374d8e4f1e637
Clover <cloverfox@protonmail.ch>
2016-08-27 23:11:51 +0100

    added git indicator

--- theme.zsh
+++ theme.zsh
@@ -1,10 +1,12 @@
-#theme related stuffs
+# theme related stuffs
 
-PROMPT="%{$fg_no_bold[yellow]%}%~ %{$reset_color%}%# "
+# get zsh-git-prompt from https://github.com/olivierverdier/zsh-git-prompt
+if [ -f $HOME/zsh-git/zsh-git-prompt/zshrc.sh ]
+then
+	. $HOME/zsh-git/zsh-git-prompt/zshrc.sh
+	PROMPT='%{$fg_no_bold[yellow]%}%~%b$(git_super_status) %# '
+else 
+	#PROMPT="%{$fg_no_bold[yellow]%}%~%{$reset_color%}%# "
+fi
+
 RPROMPT="[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]"
-
-# not used yet, for git prmompt (need the git plugin first)
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
