b892ee957b7f6584bfbb48b82d1ef0da6d628c7e
Clover <cloverfox@protonmail.ch>
2016-08-27 23:12:30 +0100

    added git indicator and changed theme to match

--- theme.zsh
+++ theme.zsh
@@ -5,8 +5,11 @@ if [ -f $HOME/zsh-git/zsh-git-prompt/zshrc.sh ]
 then
 	. $HOME/zsh-git/zsh-git-prompt/zshrc.sh
 	PROMPT='%{$fg_no_bold[yellow]%}%~%b$(git_super_status) %# '
+	#override prefix to add a space, this means we dont get a double space from the start ant the end
+	ZSH_THEME_GIT_PROMPT_PREFIX=" ("
 else 
-	#PROMPT="%{$fg_no_bold[yellow]%}%~%{$reset_color%}%# "
+	PROMPT="%{$fg_no_bold[yellow]%}%~%{$reset_color%}%# "
 fi
 
-RPROMPT="[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]"
+# test
+#RPROMPT="[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]"
